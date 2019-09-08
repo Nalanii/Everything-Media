3413c48a9c9dd14c1d78204023c475813fbc6ec5
Nalani <n.storycollege@gmail.com>
2019-09-07 20:19:55 -0400

    TV Shows Working Properly

--- Javascript/tv.js
+++ Javascript/tv.js
@@ -7,14 +7,12 @@ $(document).ready(() => {
 });
 
 function getShows(searchText) {
-    alert("Here");
     if (searchText != undefined) {
         $.get("http://www.omdbapi.com/?s=" + searchText + "&type=series&apikey=16c987d3")
             .then(function(response) {
                 var moviesString = JSON.stringify(response);
                 var data = JSON.parse(moviesString);
                 var output = "";
-                alert("here2");
 
                 for (index = 0; index < data.Search.length; index++) {
                     output += `
@@ -22,8 +20,8 @@ function getShows(searchText) {
                     <div class="well text-center">
                         <img src="${data.Search[index].Poster}">
                         <h5>${data.Search[index].Title}</h5>
-                        <a onclick="showSelected('${data.Search[index].imdbID}')" class="btn btn-primary" href="#">Show Details</a>
-                        <a onclick="showSelected('${data.Search[index].imdbID}')" class="btn btn-primary" href="#">Add to List</a>
+                        <a onclick="movieSelected('${data.Search[index].imdbID}')" class="btn btn-primary" href="#">Show Details</a>
+                        <a onclick="movieSelected('${data.Search[index].imdbID}')" class="btn btn-primary" href="#">Add to List</a>
                     </div>
                     </div>
                     `;
@@ -36,8 +34,49 @@ function getShows(searchText) {
     }
 }
 
-function showSelected(id) {
+function movieSelected(id) {
     sessionStorage.setItem('imdbID', id);
-    window.location = 'Movie&TVDetails.html';
+    window.location = 'TV_Show_Details.html';
     return false;
+}
+
+function getShow() {
+    var id = sessionStorage.getItem('imdbID');
+    $.get("http://www.omdbapi.com/?i=" + id + "&apikey=16c987d3")
+        .then(function(response) {
+            var movieString = JSON.stringify(response);
+            var data = JSON.parse(movieString);
+            output = `
+          <div class="row">
+            <div class="col-md-4">
+              <img src="${data.Poster}" class="thumbnail">
+            </div>
+            <div class="col-md-8">
+              <h2>${data.Title}</h2>
+              <ul class="list-group">
+                <li class="list-group-item"><strong>Genre:</strong> ${data.Genre}</li>
+                <li class="list-group-item"><strong>Released:</strong> ${data.Released}</li>
+                <li class="list-group-item"><strong>Rated:</strong> ${data.Rated}</li>
+                <li class="list-group-item"><strong>IMDB Rating:</strong> ${data.imdbRating}</li>
+                <li class="list-group-item"><strong>Director:</strong> ${data.Director}</li>
+                <li class="list-group-item"><strong>Writer:</strong> ${data.Writer}</li>
+                <li class="list-group-item"><strong>Actors:</strong> ${data.Actors}</li>
+              </ul>
+            </div>
+          </div>
+          <div class="row">
+            <div class="well">
+              <h3>Plot</h3>
+              ${data.Plot}
+              <hr>
+              <a href="http://imdb.com/title/${data.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+              <a href="TV_Shows.html" class="btn btn-default">Go Back To Search</a>
+            </div>
+          </div>
+        `;
+            $('#show').html(output);
+        })
+        .catch((err) => {
+            alert(str(err));
+        });
 }
